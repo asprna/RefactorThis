7418c5b8c8e9baff6aac20719cef22c493bfc072
Asanka Nawagamuwage <asanka.nawagamuwage@securepay.com.au>
2021-10-09 21:50:57 +1100

    Opject Validation Unit Tests

---
+++ UnitTest/Application/Validator/ProductOptionValidatorTest.cs
@@ -0,0 +1,39 @@
+﻿using Application.ProductOptions;
+using Application.Products;
+using Domain;
+using FluentValidation.TestHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTest.Application.Validator
+{
+	public class ProductOptionValidatorTest
+	{
+		private readonly ProductOptionValidator _productOptionValidator;
+
+		public ProductOptionValidatorTest()
+		{
+			_productOptionValidator = new ProductOptionValidator();
+		}
+
+		[Fact]
+		public void Product_AllFieldsRequired()
+		{
+			//Arrange
+			var productOption = new ProductOption { Name = null };
+
+			//Act
+			var result = _productOptionValidator.TestValidate(productOption);
+
+			//Assert
+			result.ShouldHaveValidationErrorFor(p => p.Id);
+			result.ShouldHaveValidationErrorFor(p => p.ProductId);
+			result.ShouldHaveValidationErrorFor(p => p.Description);
+			result.ShouldHaveValidationErrorFor(p => p.Name);
+		}
+	}
+}
---
+++ UnitTest/Application/Validator/ProductValidatorTest.cs
@@ -0,0 +1,40 @@
+﻿using Application.Products;
+using Domain;
+using FluentValidation.TestHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTest.Application.Validator
+{
+	public class ProductValidatorTest
+	{
+		private readonly ProductValidator _productValidator;
+
+		public ProductValidatorTest()
+		{
+			_productValidator = new ProductValidator();
+		}
+
+		[Fact]
+		public void Product_AllFieldsRequired()
+		{
+			//Arrange
+			var product = new Product { Name = null };
+
+			//Act
+			var result = _productValidator.TestValidate(product);
+
+			//Assert
+			result.ShouldHaveValidationErrorFor(p => p.Id);
+			result.ShouldHaveValidationErrorFor(p => p.Name);
+			result.ShouldHaveValidationErrorFor(p => p.Description);
+			result.ShouldHaveValidationErrorFor(p => p.Price);
+			result.ShouldHaveValidationErrorFor(p => p.DeliveryPrice);
+
+		}
+	}
+}
